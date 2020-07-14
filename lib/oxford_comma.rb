7c77413789e621d5743d68d7758a470ee972e369
Jorge Venegas <brangelina01@gmail.com>
2020-07-14 22:41:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma (array)
   if array = ["k", "i", "w", "i"].join
-    return  array_x = array = ["k", "i", "w", "i"].join + array[-1] =" and durian"
+    puts  array_x = array = ["k", "i", "w", "i"].join + array[-1] =" and durian"
   end 
 end 
 
